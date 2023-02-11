3dd2d4ec16f428d3572fb64e3bfe0f26d5bb0528
Ragapriya <ragapriya2022@outlook.com>
2023-02-11 13:43:59 +0000

    commit2

--- app/.github/workflows/scripts.sh
+++ app/.github/workflows/scripts.sh
@@ -1,1 +1,15 @@
-az ad sp create-for-rbac --name storageaccraga --role owner --scopes /subscriptions/f6995460-78d3-4278-a1d6-ea0cd0a6280c --sdk-auth
+az ad sp create-for-rbac --name "storageaccraga" --role owner --scopes /subscriptions/70d4d8f2-9121-43a2-a138-6dbb5d661809 --sdk-auth
+
+
+{
+  "clientId": "7b1ba85b-4f98-4585-a50f-78c94d22d144",
+  "clientSecret": "pXw8Q~joakZ1wDduakAVSpxmS.~fhtbmuNujtbIy",
+  "subscriptionId": "70d4d8f2-9121-43a2-a138-6dbb5d661809",
+  "tenantId": "f6995460-78d3-4278-a1d6-ea0cd0a6280c",
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  "resourceManagerEndpointUrl": "https://management.azure.com/",
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  "galleryEndpointUrl": "https://gallery.azure.com/",
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
--- providers (2).tf
+++ providers (2).tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "35b269d2-7c5c-4f36-abf1-56099fd97991"
+  subscription_id = "f6995460-78d3-4278-a1d6-ea0cd0a6280c"
   tenant_id       = "1df39095-a0f4-4b9f-abcf-e96d3abd9282"
 }
